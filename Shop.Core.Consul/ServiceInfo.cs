5a0682850037a0277b4fb8918bb26520b1fd1632
Alex Hong <yifenghung123@gmail.com>
2022-02-17 02:07:45 +0800

    #001 Add consul sample code Shop.Core

---
+++ Shop.Core.Consul/ServiceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Consul
+{
+    public class ServiceInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public string HealthCheckAddress { get; set; }
+    }
+}
---
+++ Shop.Core.WebApi/Startup.cs
@@ -0,0 +1,58 @@
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Shop.Core.Consul.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Core.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Register consul client
+            services.AddSingleton<IConsulClient>(new ConsulClient(x=> {
+
+                x.Address = new Uri(Configuration["consul:clientAddress"]);
+            }));
+
+            //Register consul service when app start
+            services.AddHostedService<ConsulRegisterService>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
